4d802652f5bf6bd6766278190b023275dcbe9599
brianbunke <brianbunke+github@gmail.com>
2016-11-26 19:25:41 -0800

    Condense repeated code
    Initial unit/integration split worked; see if cleaning up the code
    changes anything

--- Tests/build.ps1
+++ Tests/build.ps1
@@ -5,24 +5,19 @@ Write-Host "Author: $env:APPVEYOR_REPO_COMMIT_AUTHOR"
 # https://github.com/pester/Pester/wiki/Showing-Test-Results-in-CI-(TeamCity,-AppVeyor)
 # https://www.appveyor.com/docs/running-tests/#build-worker-api
 
-# Invoke-Pester runs all .Tests.ps1 in the order found by "Get-ChildItem -Recurse"
-$TestResults = Invoke-Pester -Tag 'unit' -OutputFormat NUnitXml -OutputFile ".\TestResults.xml" -PassThru
-# Upload the XML file of test results to the current AppVeyor job
-(New-Object 'System.Net.WebClient').UploadFile(
-    "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
-    (Resolve-Path ".\TestResults.xml") )
-# If a Pester test failed, use "throw" to end the script here, before deploying
-If ($TestResults.FailedCount -gt 0) {
-    throw "$($TestResults.FailedCount) Pester test(s) failed."
+# Make this a stupid quick function to avoid repeating the code twice
+function Pester-AppVeyor ($Tag) {
+    # Invoke-Pester runs all .Tests.ps1 in the order found by "Get-ChildItem -Recurse"
+    $TestResults = Invoke-Pester -Tag $Tag -OutputFormat NUnitXml -OutputFile ".\TestResults.xml" -PassThru
+    # Upload the XML file of test results to the current AppVeyor job
+    (New-Object 'System.Net.WebClient').UploadFile(
+        "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
+        (Resolve-Path ".\TestResults.xml") )
+    # If a Pester test failed, use "throw" to end the script here, before deploying
+    If ($TestResults.FailedCount -gt 0) {
+        throw "$($TestResults.FailedCount) Pester test(s) failed."
+    }
 }
 
-# Invoke-Pester runs all .Tests.ps1 in the order found by "Get-ChildItem -Recurse"
-$TestResults = Invoke-Pester -Tag 'integration' -OutputFormat NUnitXml -OutputFile ".\TestResults.xml" -PassThru
-# Upload the XML file of test results to the current AppVeyor job
-(New-Object 'System.Net.WebClient').UploadFile(
-    "https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)",
-    (Resolve-Path ".\TestResults.xml") )
-# If a Pester test failed, use "throw" to end the script here, before deploying
-If ($TestResults.FailedCount -gt 0) {
-    throw "$($TestResults.FailedCount) Pester test(s) failed."
-}
+Pester-AppVeyor -Tag 'unit'
+Pester-AppVeyor -Tag 'integration'
